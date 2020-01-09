4d1cb5544473a0ef6ed8b40e74c280a08f3a1c5a
17007003 <17007003@e214m10.istic.univ-rennes1.fr>
2020-01-09 14:48:57 +0100

    création des classes Participant, Allergie et Preference

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 
+import TP2.Participant;
+
 public class JpaTest {
 
 	/**
@@ -17,7 +19,37 @@ public class JpaTest {
 
 
 		try {
-
+			
+			Participant p1 = new Participant();
+			p1.setMail("p1@mail.com");
+			p1.setPrenom("p1");
+			p1.setNom("Np1");
+			manager.persist(p1);
+			
+			Participant p2 = new Participant();
+			p2.setMail("p2@mail.com");
+			p2.setPrenom("p2");
+			p2.setNom("Np2");
+			manager.persist(p2);
+			
+			Participant p3 = new Participant();
+			p3.setMail("p3@mail.com");
+			p3.setPrenom("p3");
+			p3.setNom("Np3");
+			manager.persist(p3);
+			
+			Participant p4 = new Participant();
+			p4.setMail("p4@mail.com");
+			p4.setPrenom("p4");
+			p4.setNom("Np4");
+			manager.persist(p4);
+			
+			Participant p5 = new Participant();
+			p5.setMail("p5@mail.com");
+			p5.setPrenom("p5");
+			p5.setNom("Np5");
+			manager.persist(p5);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
